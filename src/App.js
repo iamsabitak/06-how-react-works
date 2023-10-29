398b9bf6224490ab76f1ddc4d1be76355f5bc751
sabita <sabitakhadka091@gmail.com>
2023-10-29 13:48:42 +0545

    implemented some undo buttons

--- src/App.js
+++ src/App.js
@@ -19,14 +19,13 @@ const content = [
 ];
 
 export default function App() {
-
   return (
     <div>
       <Tabbed content={content} />
     </div>
   );
 }
-console.log(<DifferentContent test={47}/>);
+// console.log(<DifferentContent test={47} />);
 function Tabbed({ content }) {
   const [activeTab, setActiveTab] = useState(0);
 
@@ -40,7 +39,10 @@ function Tabbed({ content }) {
       </div>
 
       {activeTab <= 2 ? (
-        <TabContent item={content.at(activeTab)} />
+        <TabContent
+          item={content.at(activeTab)}
+          key={content.at(activeTab).summary}
+        />
       ) : (
         <DifferentContent />
       )}
@@ -64,9 +66,23 @@ function TabContent({ item }) {
   const [likes, setLikes] = useState(0);
 
   function handleInc() {
-    setLikes(likes + 1);
+    setLikes((likes) => likes + 1);
   }
-
+  function handleTripleInc() {
+    setLikes((likes) => likes + 1);
+    setLikes((likes) => likes + 1);
+    setLikes((likes) => likes + 1);
+    // handleInc();
+    // handleInc();
+    // handleInc();
+  }
+  function handleUndo() {
+    setShowDetails(true);
+    setLikes(0);
+  }
+  function handleUndoLater() {
+    setTimeout(handleUndo, 2000);
+  }
   return (
     <div className="tab-content">
       <h4>{item.summary}</h4>
@@ -80,13 +96,13 @@ function TabContent({ item }) {
         <div className="hearts-counter">
           <span>{likes} ❤️</span>
           <button onClick={handleInc}>+</button>
-          <button>+++</button>
+          <button onClick={handleTripleInc}>+++</button>
         </div>
       </div>
 
       <div className="tab-undo">
-        <button>Undo</button>
-        <button>Undo in 2s</button>
+        <button onClick={handleUndo}>Undo</button>
+        <button onClick={handleUndoLater}>Undo in 2s</button>
       </div>
     </div>
   );
